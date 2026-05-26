385b1786e992c2e2d8420f12a6c8946a637562c7
gibranlp <root@gibranlp.dev>
2026-05-26 16:51:17 -0500

    feat: add docker sdk client

---
+++ backend/docker/client_test.go
@@ -0,0 +1,15 @@
+package docker
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClient(t *testing.T) {
+	client, err := NewClient()
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+	if client != nil {
+		client.Close()
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"github.com/gibranlp/tentacl/backend/api"
+	"github.com/gibranlp/tentacl/backend/docker"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	cli, err := docker.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
 	e := echo.New()
 
 	// Register all routes
